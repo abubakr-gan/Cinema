6cf49263b473e60f6816026f3ae57787fdbe5cb8
Abubakr <ganiev.abubakr.2007@gmail.com>
2023-10-31 17:34:53 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const search =document.querySelector('search')
+const body =document.querySelector('body')
+
+search.addEventListener('click', function (e) {
+    e.stopPropagation();
+    this.classList.add('search--active')
+})
+body.addEventListener('click', function() {
+    this.classList.remove('search--active');
+})
